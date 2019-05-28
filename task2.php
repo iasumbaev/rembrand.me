438c9e2bf5795ff6536c8fe769f8e48af9d25320
iasumbaev <37927227+iasumbaev@users.noreply.github.com>
2019-05-28 11:52:45 +0700

    Update task2.php

--- task2.php
+++ task2.php
@@ -7,8 +7,16 @@ if (isset($_POST)) {
     $currentDT = $currentDT->format('d-m-Y H:i:s');
     file_put_contents("logs.log", "\n" . $currentDT . ' SUCCESS: get POST request.', FILE_APPEND);
     $xlsx = new XlsxWork();
-    $xlsx->addData($_POST);
-    $xlsx->saveXlsx();
+    $status = true;
+    try {
+        $xlsx->addData($_POST);
+    } catch (Exception $e) {
+        $currentDT = new DateTime();
+        $currentDT = $currentDT->format('d-m-Y H:i:s');
+        file_put_contents("logs.log", "\n" . $currentDT . ' ERROR: ' . $e, FILE_APPEND);
+        $status = false;
+    }
+    if($status) {
+        $xlsx->saveXlsx();
+    }
 }
-
-
