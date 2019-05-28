b63e22e43173419c00cb494a66422ffa2c270d61
iasumbaev <37927227+iasumbaev@users.noreply.github.com>
2019-05-28 11:50:10 +0700

    Update sendPost.php

--- sendPost.php
+++ sendPost.php
@@ -1,27 +1,29 @@
 <?php
 $url = 'http://' . $_SERVER['SERVER_ADDR'] . '/task2.php';
-$data = [
-    'number' => [
-        1,
-        2.0
-    ],
-    'date' => [
-        '11.05.1995',
-        '11.05.1996',
-    ],
-    'price' => [
-        '890.9856',
-        '890.9856',
-        '5432144684.823',
-        5423.546,
-        8012.15,
-        1890.9856,
-        890.56
 
+$data = [
+
+    'columns' => [
+        [
+            'type' => 'number',
+            'title' => 'Число'
+        ],
+        [
+            'type' => 'date',
+            'title' => 'Дата'
+        ],
+        [
+            'type' => 'price',
+            'title' => 'Цена'
+        ],
+        [
+            'type' => 'string',
+            'title' => 'Строка'
+        ],
     ],
-    'string' => [
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium at beatae dolorem eveniet explicabo itaque iure laborum libero maiores minima nam odit optio, praesentium quaerat quam vel. Deleniti, ipsa.',
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus accusantium blanditiis consequatur distinctio dolore dolores doloribus eligendi et facilis libero minus modi nostrum omnis quae, recusandae repellendus suscipit tenetur.',
+    'rows' => [
+        [1, '11.05.1995', 890.9856, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium at beatae dolorem eveniet explicabo itaque iure laborum libero maiores minima nam odit optio, praesentium quaerat quam vel. Deleniti, ipsa.'],
+        [2.0, '11.05.1996', 5423.546, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus accusantium blanditiis consequatur distinctio dolore dolores doloribus eligendi et facilis libero minus modi nostrum omnis quae, recusandae repellendus suscipit tenetur.'],
     ]
 ];
 
@@ -40,3 +42,4 @@ $options = array(
 
 $context = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
+print_r($result);
